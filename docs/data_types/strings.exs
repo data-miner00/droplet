ef8562eb094d0751e16a156bb687722b8b7f8050
data-miner00 <data-miner00@users.noreply.github.com>
2023-10-17 19:14:52 +0800

    SstringsS

--- docs/data_types/strings.exs
+++ docs/data_types/strings.exs
@@ -6,9 +6,9 @@ IO.puts String.length(euro) # 1 characters long
 
 # Concatenate strings
 firstname = "Josh"
-lastname = " Boxer"
+lastname = "Boxer"
 
-IO.puts firstname <> lastname
+IO.puts firstname <> ' ' <> lastname
 
 # String interpolation
 IO.puts "Hello, #{firstname}, your last name is #{lastname}"
@@ -23,3 +23,19 @@ IO.puts interpolated # `Test ☺☻♥` (It will turned into unicode characters)
 # The <<>> is a sequence of bytes
 # We can concatenate bytes with the concatenate operator `<>` too
 firstname_concat = firstname <> <<1, 2, 3>>
+
+# Destructure a string
+"F" <> rest = "Firewall"
+IO.puts(rest) # irewall
+
+# Destructuring a string into decimal representation
+<<head, rest::binary>> = "Firewall"
+
+# Pattern match with offset of 2
+<<head::binary-size(2), rest::binary>> = "Firewall"
+
+# List of characters are enclosed with single quotes '
+'hello' ++ 'world'
+
+# Proof that list of characters is a list
+is_list('hello world')
